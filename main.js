5df64c84e3cbe81ce3d5e51e625910612d0b6e03
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-04-13 18:28:20 +0000

    Completed main.js to add color change and number validation functionality

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function colorChange() {
+  const colorChangeEls = document.querySelectorAll('.color-change')
+
+  for (const el of colorChangeEls) {
+    el.classList.add('red');
+  }
+}
+
+document.querySelector('.color-changer').addEventListener('click', colorChange);
+
+function validateForm(evt) {
+  evt.preventDefault();
+
+  const numberInput = document.querySelector('#number-input');
+  const numberNumber = Number(numberInput.value);
+
+  const formFeedback = document.querySelector('#formFeedback');
+  if (isNaN(numberNumber) || numberNumber >= 10) {
+    formFeedback.innerText = 'Please enter a smaller number';
+  } else {
+    formFeedback.innerText = 'You are good to go!';
+  }
+}
+
+document.querySelector('.number-form').addEventListener('submit', validateForm);
